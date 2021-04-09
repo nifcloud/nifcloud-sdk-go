cae3819f763f916ca58d89254c7817a852e6f2e6
tunakyonn <tunakyonn@gmail.com>
2021-04-09 17:11:41 +0900

    Add WaitUntilVolumeAttached

--- service/computing/api_waiters.go
+++ service/computing/api_waiters.go
@@ -1372,6 +1372,45 @@ func (c *Client) WaitUntilSnapshotNormal(ctx context.Context, input *NiftyDescri
 	return w.Wait(ctx)
 }
 
+// WaitUntilVolumeAttached uses the computing API operation
+// DescribeVolumes to wait for a condition to be met before returning.
+// If the condition is not met within the max attempt window, an error will
+// be returned.
+//
+// The context must be non-nil and will be used for request cancellation. If
+// the context is nil a panic will occur. In the future the SDK may create
+// sub-contexts for http.Requests. See https://golang.org/pkg/context/
+// for more information on using Contexts.
+func (c *Client) WaitUntilVolumeAttached(ctx context.Context, input *DescribeVolumesInput, opts ...aws.WaiterOption) error {
+	w := aws.Waiter{
+		Name:        "WaitUntilVolumeAttached",
+		MaxAttempts: 40,
+		Delay:       aws.ConstantWaiterDelay(20 * time.Second),
+		Acceptors: []aws.WaiterAcceptor{
+			{
+				State:   aws.SuccessWaiterState,
+				Matcher: aws.PathAllWaiterMatch, Argument: "VolumeSet[].AttachmentSet[].Status",
+				Expected: "attached",
+			},
+		},
+		Logger: c.Config.Logger,
+		NewRequest: func(opts []aws.Option) (*aws.Request, error) {
+			var inCpy *DescribeVolumesInput
+			if input != nil {
+				tmp := *input
+				inCpy = &tmp
+			}
+			req := c.DescribeVolumesRequest(inCpy)
+			req.SetContext(ctx)
+			req.ApplyOptions(opts...)
+			return req.Request, nil
+		},
+	}
+	w.ApplyOptions(opts...)
+
+	return w.Wait(ctx)
+}
+
 // WaitUntilVolumeAvailable uses the computing API operation
 // DescribeVolumes to wait for a condition to be met before returning.
 // If the condition is not met within the max attempt window, an error will
